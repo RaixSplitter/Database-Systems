196ad4bbed7cc055fd2534e6fd59bd7afa5ae5a7
RaixSplitter <markus.bao.quan@gmail.com>
2024-02-22 09:54:13 +0100

    F

--- InsuranceDB.Init.sql
+++ InsuranceDB.Init.sql
@@ -29,28 +29,28 @@ DROP TABLE IF EXISTS Person;
 -- CREATE TABLES
 
 CREATE TABLE Person
-    (DriverID       INT(8),
+    (DriverID       VARCHAR(8),
      DriverName		VARCHAR(20) NOT NULL,
      _Address		VARCHAR(50),
      PRIMARY KEY(DriverID)
     );
 
 CREATE TABLE Car
-    (License		VARCHAR(8),
-     Model			VARCHAR(20),
+    (License		CHAR(7),
+     Model			VARCHAR(30),
      ProdYear		YEAR,
      PRIMARY KEY(License)
     );
 
 CREATE TABLE Accident
-    (ReportNumber	INT(10),
+    (ReportNumber	VARCHAR(10),
      AccDate		DATE,
      _Location		VARCHAR(50),
      PRIMARY KEY(ReportNumber)
     );
 
 CREATE TABLE Owns
-    (DriverID		INT(8),
+    (DriverID		VARCHAR(8),
      License		VARCHAR(8),
      PRIMARY KEY(DriverID, License),
      FOREIGN KEY(DriverID)      REFERENCES Person(DriverID) ON DELETE CASCADE,
@@ -58,9 +58,9 @@ CREATE TABLE Owns
     );
 
 CREATE TABLE Participants
-    (ReportNumber	INT(10),
-     License		VARCHAR(8),
-     DriverID		INT(8),
+    (ReportNumber	VARCHAR(10),
+     License		CHAR(7),
+     DriverID		VARCHAR(8),
      DamageAmount	DECIMAL(8,2),
      PRIMARY KEY(ReportNumber, License),
      FOREIGN KEY(ReportNumber)  REFERENCES Accident(ReportNumber) ON DELETE CASCADE,
@@ -69,3 +69,25 @@ CREATE TABLE Participants
     );
 
 
+-- Insert the following
+-- Person(31262549, Hans Hansen, Jernbane Alle 74, 2720 Vanløse)
+
+-- Car(JW46898, Honda Accord Aut. 2.0, 2001)
+
+-- Accident(3004000121, 2015-06-18, 2605 Brøndby)
+
+-- Owns(31262549, JW46898)
+
+-- Participants(3004000121, JW46898, 31262549, 6800.00)
+
+INSERT INTO Person VALUES (31262549, 'Hans Hansen', 'Jernbane Alle 74, 2720 Vanløse');
+
+INSERT INTO Car VALUES ('JW46898', 'Honda Accord Aut. 2.0', 2001);
+
+INSERT INTO Accident VALUES ('3004000121', '2015-06-18', '2605 Brøndby');
+
+INSERT INTO Owns VALUES (31262549, 'JW46898');
+
+INSERT INTO Participants VALUES (3004000121, 'JW46898', 31262549, 6800.00);
+
+SELECT * FROM Person NATURAL JOIN car, owns, participants, accident;
